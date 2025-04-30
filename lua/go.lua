d9d4a718146b4ab6632c4eff712c1a1ae27f5911
Konrad Reiche <konrad.reiche@gmail.com>
2025-04-30 13:40:08 -0700

    Run optional Go formatter defined by GO_EXTRA_FMT

--- lua/go.lua
+++ lua/go.lua
@@ -6,3 +6,24 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
   end
 })
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.go',
+  callback = function()
+    local tool = vim.fn.getenv("GO_EXTRA_FMT")
+    if type(tool) == "string" and tool ~= "" then
+      local file_path = vim.fn.expand('%:p')
+      local cmd = vim.fn.split(tool, " ")
+      table.insert(cmd, file_path)
+
+      if vim.fn.executable(cmd[1]) == 1 then
+        vim.fn.system(cmd)
+        vim.api.nvim_buf_call(0, function()
+          vim.cmd('checktime')
+        end)
+      else
+        vim.notify("Executable not found: " .. cmd[1], vim.log.levels.WARN)
+      end
+    end
+  end,
+})
